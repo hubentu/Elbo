7934ff4920005d8f299f6f604ff3d66c260979e2
qhu <qhu@local>
2016-10-14 17:25:47 -0400

    update unit test
    - options(useFancyQuotes=FALSE)

--- tests/testthat/test_utlities.R
+++ tests/testthat/test_utlities.R
@@ -6,3 +6,18 @@ test_that("is_scalar_character() works", {
     expect_false(is_scalar_character(NA_character_))
     expect_false(is_scalar_character(""))
     })
+
+test_that("hi() starts a conversation", {
+    expect_identical(
+      hi("world"),
+      "hello WORLD you have 5 letters in your name!"
+    )
+    expect_identical(
+      hi("world", "whisper"),
+      "hello world you have 5 letters in your name!"
+    )
+    expect_error(
+      hi("world", how="murmur"),
+      "'arg' should be one of \"shout\", \"whisper\"" #options(useFancyQuotes = FALSE)
+      )
+})
